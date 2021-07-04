c6b8124dad304667995070208f792d49913c1235
iyu <iyu@vlapwd.com>
2021-07-04 14:23:05 +0900

    render

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = "hoge"
-const element = <h1>Hello, {name}</h1>
+function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
 
-ReactDOM.render(
-    element,
-    document.getElementById('app')
-)
+//毎回render読んでるけど実際更新されるのは内容が変更されているDateだけ
+setInterval(tick, 1000);
