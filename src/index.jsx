a8c1295dcf03890ba6c96ef015a2c4b8693245b9
iyu <iyu@vlapwd.com>
2021-07-04 17:40:51 +0900

    stateとライフサイクル

--- src/index.jsx
+++ src/index.jsx
@@ -1,67 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function Avatar(props){
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
     return (
-        <img className="Avatar"
-            src={props.user.avatarUrl}
-            alt={props.user.name}
-        />
-    )
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
-function UserInfo(props){
-    return (
-        <div className="UserInfo">
-            <Avatar user={props.user} />
-            <div className="UserInfo-name">
-                {props.user.name}
-            </div>
-        </div>
-    )
-}
-
-function Comment(props) {
-    return(
-        <div className="Comment">
-            <UserInfo user={props.author} />
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {props.date}
-            </div>
-        </div>
-    )
-}
-
-// function Comment(props) {
-//     return (
-//       <div className="Comment">
-//         <div className="UserInfo">
-//           <img className="Avatar"
-//             src={props.author.avatarUrl}
-//             alt={props.author.name}
-//           />
-//           <div className="UserInfo-name">
-//             {props.author.name}
-//           </div>
-//         </div>
-//         <div className="Comment-text">
-//           {props.text}
-//         </div>
-//         <div className="Comment-date">
-//           {formatDate(props.date)}
-//         </div>
-//       </div>
-//     );
-//   }
-const data = {
-    name:"hoge",
-    avatarUrl:"https://1.bp.blogspot.com/-8FUEz6vBnoQ/X7zMVAuhQMI/AAAAAAABcZ0/VI1Z9eN76pIj2rfHshveNbFoMKubXYTpACNcBGAsYHQ/s400/baby_role_towel_utsubuse.png"
-}
-
-ReactDOM.render(
-    <Comment　author={data} date={new Date().toLocaleTimeString()} />,
-    document.getElementById('root')
-);
+ReactDOM.render(<Clock />, document.getElementById("root"));
