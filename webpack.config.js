bce429f4b7b6ac806489ab0849df518158730507
iyu <iyu@vlapwd.com>
2021-07-16 00:35:43 +0900

    いいかげん面倒なのでホットリロードが欲しい

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    entry: './src/index.jsx',
-    output: {
-        path: __dirname + '/dist/js',
-        filename: 'main.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.js[x]?$/, 
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  '@babel/preset-env',
-                  '@babel/preset-react'
-                ],
-                plugins: ['@babel/plugin-syntax-jsx']
-              }
-            }
-          }
-        ]
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.jsx",
+  output: {
+    path: __dirname + "/dist/js",
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-syntax-jsx"],
+          },
+        },
       },
-      resolve: {
-        extensions: ['.js', '.jsx', '.json']
-      }
-}
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  devServer: {
+    contentBase: "./dist",
+    //追加したはいいけどwebsocket飛んでなくて効いてない。なんで？
+    hot: true,
+  },
+};
