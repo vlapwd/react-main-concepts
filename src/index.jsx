895618ef42d3731e69c5391519b4746cfd727ed0
iyu <iyu@vlapwd.com>
2021-08-06 03:06:41 +0900

    stateのリフトアップ

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,7 @@ import { NameForm } from "./nameForm";
 import { EssayForm } from "./essayForm";
 import { FlavorForm } from "./flavorForm";
 import { Reservation } from "./reservation";
+import { Calculator } from "./calculator";
 
 const messages = ["React", "Re: React", "Re:Re: React"];
 const numbers = [1, 2, 3, 4, 5];
@@ -25,8 +26,9 @@ const element = (
     <EssayForm />
     <FlavorForm />
     <input type="file" />
-    https://ja.reactjs.org/docs/uncontrolled-components.html#the-file-input-tag
+    {/* https://ja.reactjs.org/docs/uncontrolled-components.html#the-file-input-tag */}
     <Reservation />
+    <Calculator />
   </div>
 );
 ReactDOM.render(element, document.getElementById("root"));
