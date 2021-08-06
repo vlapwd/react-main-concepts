e72920502438c5afa404ca5aee0545315683a89b
iyu <iyu@vlapwd.com>
2021-08-07 01:43:42 +0900

    css loader追加

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,17 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { url: false }
+          }
+        ]
+      }
     ],
   },
   resolve: {
